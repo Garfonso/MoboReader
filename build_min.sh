99fe5c16c782d4d17f40777403774cf2ff71f703
Achim Königs <garfonso@mobo.info>
2014-09-30 15:30:12 +0200

    updated build script

--- build_min.sh
+++ build_min.sh
@@ -9,6 +9,8 @@ cp appinfo.json deploy
 scp -r -P 55022 deploy/* root@localhost:/media/cryptofs/apps/usr/palm/applications/info.mobo.moboreader/
 scp -r -P 55022 service/* root@localhost:/media/cryptofs/apps/usr/palm/services/info.mobo.moboreader.storage/
 
+ssh -p 55022 root@localhost systemctl restart luna-next
+
 palm-package package deploy service
 palm-install *.ipk
-scp -P 55022 info.mobo.moboreader_0.0.3_all.ipk root@localhost:/media/internal
+scp -P 55022 info.mobo.moboreader_*_all.ipk root@localhost:/media/internal
