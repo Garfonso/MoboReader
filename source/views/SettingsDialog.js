b0f0e5704707c11fbcdaa7b3d7460d886aef630b
--replace-all <garfonso@mobo.info>
2015-01-23 14:56:25 +0100

    hide maxDownloadedArticles on webOS.

--- source/views/SettingsDialog.js
+++ source/views/SettingsDialog.js
@@ -1,137 +1,141 @@
+/*jslint sloppy: true*/
+/*global enyo*/
+
 enyo.kind({
-    name: "moboreader.SettingsDialog",
-    kind: "onyx.Popup",
-    style: "text-align: center; width: 80%; height: 80%;",
-    scrim: true,
-    modal: true,
-    autoDismiss: true,
-    floating: true,
-    centered: true,
-    showTransitions: true,
-    events: {
-        onLogoutRequest: ""
-    },
-    bindings: [
-        { from: "^.moboreader.Prefs.maxDownloadedArticles", to: "$.maxEntries.inVal", oneWay: false},
-        {from: "^.moboreader.Prefs.sortOrder", to: "$.sortOrder.inVal", oneWay: false  },
+	name: "moboreader.SettingsDialog",
+	kind: "onyx.Popup",
+	style: "text-align: center; width: 80%; height: 80%;",
+	scrim: true,
+	modal: true,
+	autoDismiss: true,
+	floating: true,
+	centered: true,
+	showTransitions: true,
+	events: {
+		onLogoutRequest: ""
+	},
+	bindings: [
+		{ from: "^.moboreader.Prefs.maxDownloadedArticles", to: "$.maxEntries.inVal", oneWay: false},
+		{ from: "^.ArticleContentHandler.isWebOS", to: "$.maxEntries.showing", transform: function (val) { return !val; } },
+		{from: "^.moboreader.Prefs.sortOrder", to: "$.sortOrder.inVal", oneWay: false  },
 
-        {from: "^.moboreader.Prefs.fontSize", to: "$.fontSize.inVal", oneWay: false  },
-        {from: "^.moboreader.Prefs.goBackOnArchive", to: "$.goBackOnArchive.checked", oneWay: false  },
+		{from: "^.moboreader.Prefs.fontSize", to: "$.fontSize.inVal", oneWay: false  },
+		{from: "^.moboreader.Prefs.goBackOnArchive", to: "$.goBackOnArchive.checked", oneWay: false  },
 
-        {from: "^.moboreader.Prefs.useSpritz", to: "$.useSpritz.checked", oneWay: false  },
-        {from: "^.moboreader.Prefs.downloadSpritzOnUpdate", to: "$.downloadSpritzOnUpdate.checked", oneWay: false  }
-    ],
-    components: [
-        {
-            kind: "enyo.Scroller",
-            touch: true,
-            thumb: false,
-            style: "height: 100%",
-            components: [
-                {
-                    kind: "FittableRows",
-                    style: "height: 550px;",
-                    components: [
-                        {
-                            kind: "onyx.Groupbox",
-                            components: [
-                                {
-                                    kind: "onyx.GroupboxHeader",
-                                    content: "Pocket settings"
-                                },
-                                {
-                                    kind: "PickerWithHintAndBinding",
-                                    name: "maxEntries",
-                                    hint: "Max downloaded entries",
-                                    pickerComponents: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
-                                },
-                                {
-                                    kind: "PickerWithHintAndBinding",
-                                    name: "sortOrder",
-                                    hint: "Sort articles for ",
-                                    pickerComponents: ["newest", "oldest", "title", "url"]
-                                }
-                            ]
-                        },
-                        {
-                            kind: "onyx.Groupbox",
-                            components: [
-                                {
-                                    kind: "onyx.GroupboxHeader",
-                                    content: "Display settings"
-                                },
-                                {
-                                    kind: "PickerWithHintAndBinding",
-                                    name: "fontSize",
-                                    hint: "Font size in article view",
-                                    pickerComponents: [12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
-                                },
-                                {
-                                    style: "overflow: hidden; padding: 10px;",
-                                    components: [
-                                        { content: "Go back on archive", style: "float: left; "},
-                                        {
-                                            kind: "onyx.Checkbox",
-                                            name: "goBackOnArchive",
-                                            style: "float: right;"
-                                        }
-                                    ]
-                                },
-                            ]
-                        },
-                        {
-                            kind: "onyx.Groupbox",
-                            components: [
-                                {
-                                    kind: "onyx.GroupboxHeader",
-                                    content: "Spritz settings"
-                                },
-                                {
-                                    style: "overflow: hidden; padding: 10px;",
-                                    components: [
-                                        { content: "Use spritz", style: "float: left; "},
-                                        {
-                                            kind: "onyx.Checkbox",
-                                            name: "useSpritz",
-                                            style: "float: right;"
-                                        }
-                                    ]
-                                },
-                                {
-                                    style: "overflow: hidden; padding: 10px;",
-                                    components: [
-                                        { content: "Download spritz data for offline use", style: "float: left; "},
-                                        {
-                                            kind: "onyx.Checkbox",
-                                            name: "downloadSpritzOnUpdate",
-                                            style: "float: right;"
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        {
-                            style: "margin: 10px auto; display: block;",
-                            kind: "onyx.Button",
-                            content: "Logout from Pocket",
-                            name: "logoutBtn",
-                            ontap: "logout"
-                        },
+		{from: "^.moboreader.Prefs.useSpritz", to: "$.useSpritz.checked", oneWay: false  },
+		{from: "^.moboreader.Prefs.downloadSpritzOnUpdate", to: "$.downloadSpritzOnUpdate.checked", oneWay: false  }
+	],
+	components: [
+		{
+			kind: "enyo.Scroller",
+			touch: true,
+			thumb: false,
+			style: "height: 100%",
+			components: [
+				{
+					kind: "FittableRows",
+					style: "height: 550px;",
+					components: [
+						{
+							kind: "onyx.Groupbox",
+							components: [
+								{
+									kind: "onyx.GroupboxHeader",
+									content: "Pocket settings"
+								},
+								{
+									kind: "PickerWithHintAndBinding",
+									name: "maxEntries",
+									hint: "Max downloaded entries",
+									pickerComponents: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+								},
+								{
+									kind: "PickerWithHintAndBinding",
+									name: "sortOrder",
+									hint: "Sort articles for ",
+									pickerComponents: ["newest", "oldest", "title", "url"]
+								}
+							]
+						},
+						{
+							kind: "onyx.Groupbox",
+							components: [
+								{
+									kind: "onyx.GroupboxHeader",
+									content: "Display settings"
+								},
+								{
+									kind: "PickerWithHintAndBinding",
+									name: "fontSize",
+									hint: "Font size in article view",
+									pickerComponents: [12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
+								},
+								{
+									style: "overflow: hidden; padding: 10px;",
+									components: [
+										{ content: "Go back on archive", style: "float: left; "},
+										{
+											kind: "onyx.Checkbox",
+											name: "goBackOnArchive",
+											style: "float: right;"
+										}
+									]
+								}
+							]
+						},
+						{
+							kind: "onyx.Groupbox",
+							components: [
+								{
+									kind: "onyx.GroupboxHeader",
+									content: "Spritz settings"
+								},
+								{
+									style: "overflow: hidden; padding: 10px;",
+									components: [
+										{ content: "Use spritz", style: "float: left; "},
+										{
+											kind: "onyx.Checkbox",
+											name: "useSpritz",
+											style: "float: right;"
+										}
+									]
+								},
+								{
+									style: "overflow: hidden; padding: 10px;",
+									components: [
+										{ content: "Download spritz data for offline use", style: "float: left; "},
+										{
+											kind: "onyx.Checkbox",
+											name: "downloadSpritzOnUpdate",
+											style: "float: right;"
+										}
+									]
+								}
+							]
+						},
+						{
+							style: "margin: 10px auto; display: block;",
+							kind: "onyx.Button",
+							content: "Logout from Pocket",
+							name: "logoutBtn",
+							ontap: "logout"
+						},
 
-                        {
-                            style: "margin: 10px 5px; width: 100px;",
-                            kind: "onyx.Button",
-                            content: "Close",
-                            name: "cancelBtn",
-                            ontap: "hide"
-                        }
-                    ]
-                }
-            ]
-        }
-    ],
-    logout: function () {
-        this.hide();
-        this.doLogoutRequest();
-    }
+						{
+							style: "margin: 10px 5px; width: 100px;",
+							kind: "onyx.Button",
+							content: "Close",
+							name: "cancelBtn",
+							ontap: "hide"
+						}
+					]
+				}
+			]
+		}
+	],
+	logout: function () {
+		this.hide();
+		this.doLogoutRequest();
+	}
 });
