cd3256bda978481f897123ae3297281f36e96919
--replace-all <garfonso@mobo.info>
2015-01-23 10:28:49 +0100

    skeleton for download spritz assitant.

---
+++ service/assistants/DownloadSpritzAssitant.js
@@ -0,0 +1,11 @@
+/*global fs, Config*/
+var DownloadSpritzAssistant = function () { "use strict"; };
+
+DownloadSpritzAssistant.prototype.run = function (outerfuture) {
+	"use strict";
+	var args = this.controller.args;
+
+	//TODO: download spritz.js and jquery.js from spritzinc.
+	//TODO: download css file and other assets from spritzinc
+	//TODO: modify files so that things are loaded from filesystem.
+};
