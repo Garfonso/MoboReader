6c683f6bb54cd0ab8be4dd2a27a0370f10203d59
--replace-all <garfonso@tratschtante.de>
2015-03-07 12:52:25 +0100

    whitespace change

--- service/libraries/checkResult.js
+++ service/libraries/checkResult.js
@@ -1,3 +1,4 @@
+/*jslint node: true */
 /**
  * helper to check result of futures with catching exceptions
  * because futures can "transport" exceptions to waiting
@@ -8,13 +9,13 @@
  * @return the result, returnValue = false hints to error, check exception fiel for future exception
  */
 function checkResult(future) {
-    "use strict";
-    var exception = future.exception;
-    if (exception) {
-        return {returnValue: false, exception: future.exception};
-    }
-    //else
-    return future.result;
+	"use strict";
+	var exception = future.exception;
+	if (exception) {
+		return {returnValue: false, exception: future.exception};
+	}
+	//else
+	return future.result;
 }
 
 module.exports = checkResult;
